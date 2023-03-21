17c6ddf7a9e5b4307cb1fe01f1edda60c954afd7
Envi-Scripts <126303539+Envi-Scripts@users.noreply.github.com>
2023-03-21 21:45:49 +0000

    V1.1 - *important* - added lots of validation to prevent exploits

--- server.lua
+++ server.lua
@@ -1,29 +1,39 @@
+local Players = {}
 if Config.Framework == 'esx' then
     ESX = exports['es_extended']:getSharedObject()
+
     RegisterServerEvent('envi-dumpsterdive:rewards') 
-    AddEventHandler('envi-dumpsterdive:rewards',function(luck)
+    AddEventHandler('envi-dumpsterdive:rewards', function(luck)
         local _source = source
         local xPlayer  = ESX.GetPlayerFromId(source)
         local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
         local uncommon = Config.UncommonRewards[math.random(#Config.UncommonRewards)]
         local rare = Config.RareRewards[math.random(#Config.RareRewards)]
-        if luck <= 14 then
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-        elseif luck >= 14 and luck < 20 then
-            xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
-            xPlayer.addInventoryItem(uncommon,math.random(1, Config.MaxUncommonReward))
-        elseif luck == 20 then
-            xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
-            xPlayer.addInventoryItem(rare,math.random(1, Config.MaxRareReward))
+        local identifier = xPlayer.identifier
+
+        if not Players[identifier] or os.time() > Players[identifier] then
+            if luck <= 14 then
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+            elseif luck >= 14 and luck < 20 then
+                xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
+                xPlayer.addInventoryItem(uncommon,math.random(1, Config.MaxUncommonReward))
+            elseif luck == 20 then
+                xPlayer.addInventoryItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
+                xPlayer.addInventoryItem(rare,math.random(1, Config.MaxRareReward))
+            end
+            Players[identifier] = os.time() + Config.Cooldown
+        else
+            -- on cooldown
         end
     end)
 elseif Config.Framework == 'qb' then
     QBCore = exports["qb-core"]:GetCoreObject()
+
     RegisterServerEvent('envi-dumpsterdive:rewards') 
     AddEventHandler('envi-dumpsterdive:rewards',function(luck)
         local _source = source
@@ -31,19 +41,25 @@ elseif Config.Framework == 'qb' then
         local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
         local uncommon = Config.UncommonRewards[math.random(#Config.UncommonRewards)]
         local rare = Config.RareRewards[math.random(#Config.RareRewards)]
-        if luck <= 14 then
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-            local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
-            player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
-        elseif luck >= 14 and luck < 20 then
-            player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
-            player.Functions.AddItem(uncommon,math.random(1, Config.MaxUncommonReward))
-        elseif luck == 20 then
-            player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
-            player.Functions.AddItem(rare,math.random(1, Config.MaxRareReward))
+        local identifier = player.PlayerData.citizenid
+        
+        if not Players[identifier] or os.time() > Players[identifier] then
+            if luck <= 14 then
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+                local common = Config.CommonRewards[math.random(#Config.CommonRewards)]
+                player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/3)))
+            elseif luck >= 14 and luck < 20 then
+                player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
+                player.Functions.AddItem(uncommon,math.random(1, Config.MaxUncommonReward))
+            elseif luck == 20 then
+                player.Functions.AddItem(common,math.random(1, math.ceil(Config.MaxCommonReward/2)))
+                player.Functions.AddItem(rare,math.random(1, Config.MaxRareReward))
+            end
+        else
+            -- on cooldown
         end
     end)
 end
@@ -51,4 +67,3 @@ end
 RegisterNetEvent("envi-dumpsterdive:Server:SyncEffect", function(pos)
     TriggerClientEvent("envi-dumpsterdive:Client:SyncEffect", -1, pos)
 end)
-
